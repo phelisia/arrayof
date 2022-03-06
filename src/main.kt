0b5092cde891c69d19f598eb778d870ee87d3b2b
phelisa <phelisajeruto@gmail.com>
2022-03-06 12:48:06 +0300

    kotlin arrays

---
+++ src/main.kt
@@ -0,0 +1,38 @@
+fun main(){
+    println("hey")
+    createfun()
+    cities()
+    num()
+    var jina=name(arrayOf("phelisia","sarah","chumba"))
+    return(jina)
+}
+fun createfun (){
+    var takestring= arrayOf("phelisai","Bakari","Chumba","Sarah")
+
+    println(takestring.contentToString())
+
+}
+fun cities(){
+    var places= arrayOf("harara","mumbai","dodoma","jakarta")
+    places.sortedArray() .forEach { pj->
+        println(pj.capitalize())
+
+    }
+
+}
+fun num(){
+var numbers= arrayOf(32,17,42,13,78,43,90,31,3,73,11,158,62)
+var sum =numbers[1]+numbers[4]
+    println(sum)
+    println(numbers.indexOf(158))
+    println(numbers.sortedArray().contentToString())
+
+}
+    fun name(strings:Array<String>){
+        var leta=strings
+        println(leta.contentToString())
+
+
+
+
+}
